f34ca72f84653a25cacc77b0df3800163266e25a
samakunchan <lesdieux974>
2021-09-19 03:30:48 +0200

    ma config simplifier + qq explications

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM php:7.4-apache
 RUN docker-php-ext-install mysqli
 
 # Ci dessous, c'est xdebug, afin d'avoir un var_dump sympathique.
-# Ajouter XDEBUG_CONFIG: remote_host=host.docker.internal remote_port=9000 remote_enable=1 dans le docker-compose du service php
+# Ajouter "XDEBUG_CONFIG: remote_host=host.docker.internal remote_port=9000 remote_enable=1" dans le docker-compose du service php.
+# Ex: environment:
+#       XDEBUG_CONFIG: remote_host=host.docker.internal remote_port=9000 remote_enable=1
 RUN pecl install -f xdebug
 RUN echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini;
--- src/index.php
+++ src/index.php
@@ -1,36 +1,12 @@
 <?php
 //ini_set('xdebug.var_display_max_depth', 99);
-$mysqli = new mysqli("db", "root", "root", "app");
 
-$sql = 'SELECT * FROM nego WHERE idnego = 10000028
-    OR idnego = 10000159
-    OR idnego = 10000115
-    OR idnego = 14
-    OR idnego = 10000154
-    OR idnego = 1114
-    OR idnego = 10000152
-    OR idnego = 10000127
-    OR idnego = 10000041
-    OR idnego = 10000125
-    OR idnego = 1014
-    OR idnego = 10000001
-    OR idnego = 1116
-    OR idnego = 10000126
-    OR idnego = 10000083
-    OR idnego = 10000089
-    OR idnego = 10000092
-    OR idnego = 10000014
-    OR idnego = 10000153
-    OR idnego = 10000143
-    OR idnego = 10000146
-    OR idnego = 536
-    OR idnego = 10000054
-    OR idnego = 10000142
-    OR idnego = 10000156
-    OR idnego = 10000119
-    OR idnego = 233
-    OR idnego = 10000059
-    OR idnego = 10000138';
+/**
+ * Créer un base de données à part avant de procéder
+ */
+$mysqli = new mysqli("db", "root", "root", "appv2");
+
+$sql = 'SELECT * FROM utilisateurs ORDER BY id';
 $result = $mysqli->query($sql);
 if ($result = $mysqli->query($sql)) {
     while ($data = $result->fetch_object()) {
@@ -38,17 +14,32 @@ if ($result = $mysqli->query($sql)) {
     }
 }
 
-var_dump($users);
-//foreach ($users as $user) {
-////    var_dump($user->amdp);
-//    $hash = password_hash($user->amdp, PASSWORD_DEFAULT);
-//    $ok = $mysqli->query("UPDATE nego SET mdp= '".$hash."' WHERE idnego = ".(+$user->idnego)." ");
-//    var_dump((+$user->idnego) .': '.$user->amdp .' => '. password_hash($user->amdp, PASSWORD_DEFAULT));
-//}
-//$ok = $mysqli->query("UPDATE nego SET mdp= '57YtZ63' WHERE idnego = 14");
-//var_dump($ok);
+//var_dump($users);
 
-//var_dump(password_hash('123', PASSWORD_DEFAULT));
-//echo '<pre>';
-//echo '</pre>';
+/**
+ * Hash les mots de passe reçut et me donne la requete
+ * Charger la page pour lancer l'action
+ * Désactiver les commentaires pour charger l'action
+ */
+//foreach ($users as $user) {
+//    $hash = password_hash($user->amdp, PASSWORD_DEFAULT);
+//    $ok = $mysqli->query("UPDATE utilisateurs SET mdp= '".$hash."' WHERE id = ".(+$user->id)." ");
+////    var_dump((+$user->id) .': '.$user->amdp .' => '. $hash);
+//    var_dump("UPDATE utilisateurs SET utilisateurs.password = $hash WHERE id = $user->id");
+//}
+
+/**
+ * Générer un mot de passe à la Thierry
+ * Charger la page pour lancer l'action
+ * Désactiver les commentaires pour charger l'action
+ */
+//$motpass= '';
+//$chaine = 'abBDEFcdefghijkmnPQRSTUVWXYpqrst23456789';
+//srand((double)microtime()*1000000);
+//for($i=0; $i<6; $i++)
+//{
+//    $motpass .= $chaine[rand()%strlen($chaine)];
+//}
+//var_dump($motpass);
+
 
